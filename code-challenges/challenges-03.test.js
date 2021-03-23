18b1546a154cb5314888901aa7ed759d43729f97
mohammad ashour <moh.ash96@gmail.com>
2021-03-23 23:22:26 +0200

    passed the 6th

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -125,6 +125,8 @@ Write a function named sortByLength that takes in an array of strings and return
 
 const sortByLength = (arr) => {
   // Solution code here...
+  arr = arr.sort((a,b)=> a.length - b.length);
+  return arr
 };
 
 /* ------------------------------------------------------------------------------------------------
