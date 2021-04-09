bfcfde213b167368953f4ed3645714c7bc80cf40
mohammad ashour <moh.ash96@gmail.com>
2021-04-09 22:43:34 +0300

    finished2

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -151,10 +151,13 @@ For example:
 hasChildrenValues(characters, 'Cersei') will return true
 hasChildrenValues(characters, 'Sansa') will return false
 ------------------------------------------------------------------------------------------------ */
-
 const hasChildrenValues = (arr, character) => {
   // Solution code here...
-
+  let arr2 = [];
+  arr.forEach(obj=>{
+  obj.name === character? arr2.push(obj.children): console.log('hi');
+  })
+ 	return arr2[0] === undefined ? false: true
 };
 
 /* ------------------------------------------------------------------------------------------------
