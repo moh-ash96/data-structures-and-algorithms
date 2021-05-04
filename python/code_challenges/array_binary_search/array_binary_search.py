becdf307b55bb9f489d6a7cf55ee2277a5aac56b
moh-ash96 <moh.ash96@gmail.com>
2021-05-05 02:40:05 +0300

    finished

--- python/code_challenges/array_binary_search/array_binary_search.py
+++ python/code_challenges/array_binary_search/array_binary_search.py
@@ -1,7 +1,19 @@
 def BinarySearch(arr, key):
-    if arr == [4,8,15,16,23,42] and key == 15:
-        return 2
-    elif arr == [11,22,33,44,55,66,77] and key == 90:
-        return -1
-    elif arr == [1, 2, 3, 5, 6, 7] and key == 4:
-        return -1
+  mid = 0
+  start = 0
+  end = 0
+  for i in arr:
+    end+=1
+  if key > arr[end-1]:
+    return -1
+  else:
+    while (start <= end):
+        mid = (start + end) // 2
+        if key == arr[mid]:
+            return mid
+
+        if key < arr[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+  return -1
