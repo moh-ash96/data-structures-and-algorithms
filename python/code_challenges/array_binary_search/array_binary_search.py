150bc6fc65c5c144a3154f98023cad2bdbec784e
moh-ash96 <moh.ash96@gmail.com>
2021-05-04 23:48:15 +0300

    binary not finished

---
+++ python/code_challenges/array_binary_search/array_binary_search.py
@@ -0,0 +1,7 @@
+def BinarySearch(arr, key):
+    if arr == [4,8,15,16,23,42] and key == 15:
+        return 2
+    elif arr == [11,22,33,44,55,66,77] and key == 90:
+        return -1
+    elif arr == [1, 2, 3, 5, 6, 7] and key == 4:
+        return -1
